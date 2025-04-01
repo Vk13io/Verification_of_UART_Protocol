fb97f5d7d8ed0f05e16993da13a47c813a632b7d
Vikas K <83337729+Vk13io@users.noreply.github.com>
2025-04-01 17:02:26 +0530

    Create uart_pkg.sv

---
+++ test/uart_pkg.sv
@@ -0,0 +1,20 @@
+package uart_pkg;
+
+	import uvm_pkg::*;
+	`include "timescale.v"	
+	`include "uvm_macros.svh"
+	`include "uart_xtn.sv"
+	`include "uart_agt_cfg.sv"
+	`include "uart_config.sv"
+	`include "uart_drv.sv"
+	`include "uart_monitor.sv"
+	`include "uart_seqr.sv"
+	`include "uart_agent.sv"
+	`include "uart_seqs.sv"
+	//`include "uart_v_seqr.sv"
+        //`include "uart_v_seqs.sv"
+	`include "uart_sb.sv"
+	`include "uart_env.sv"
+	`include "uart_test.sv"
+endpackage 
+	
